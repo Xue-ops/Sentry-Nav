abf59a3701efb273b421820155616e337ceebbf2
Xue <xueli2938229644@163.com>
2026-05-24 00:05:12 -0700

    narrow path plan and control success with some collision

---
+++ sentry_nav_bringup/launch/simulation_bringup.launch.py
@@ -0,0 +1,80 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    map_params_file = LaunchConfiguration('map_params_file')
+    params_file = LaunchConfiguration('params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    default_map_params_file = PathJoinSubstitution([
+        FindPackageShare('sentry_nav_bringup'),
+        'config',
+        'savedmap_params.yaml'
+    ])
+
+    default_params_file = (
+        '/home/xli/catkin_ws/src/Sentry-Nav/navigation2/nav2_bringup/params/nav2_params.yaml'
+    )
+
+    fake_odom_map_node = Node(
+        package='fake_msg',
+        executable='fake_odom_map',
+        name='fake_odom_map',
+        output='screen',
+        parameters=[{
+            'use_sim_time': use_sim_time
+        }]
+    )
+
+    savedmap_rviz_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare('sentry_nav_bringup'),
+                'launch',
+                'savedmap_rviz_bringup.launch.py'
+            ])
+        ),
+        launch_arguments={
+            'map_params_file': map_params_file
+        }.items()
+    )
+
+    nav2_simulation = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                FindPackageShare('nav2_bringup'),
+                'launch',
+                'navigation_launch.py'
+            ])
+        ),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+            'params_file': params_file
+        }.items()
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation time for all launched nodes'
+        ),
+        DeclareLaunchArgument(
+            'map_params_file',
+            default_value=default_map_params_file,
+            description='Path to saved map bringup parameter file'
+        ),
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=default_params_file,
+            description='Path to Nav2 parameter file'
+        ),
+        fake_odom_map_node,
+        savedmap_rviz_bringup,
+        nav2_simulation,
+    ])
