94de76894048cbf94f894eaac0a4db266fdbd8e8
Xue <xueli2938229644@163.com>
2026-06-23 02:40:07 -0700

    add imu transfer

--- cpp_lidar_filter/src/dual_lidar_republisher.cpp
+++ cpp_lidar_filter/src/dual_lidar_republisher.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/imu.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 
 class DualLidarRepublisher : public rclcpp::Node
@@ -13,20 +14,30 @@ public:
     declare_parameter("front_input_topic", "/livox/lidar_192_168_1_188");
     declare_parameter("front_output_topic", "/mid360_front/lidar");
     declare_parameter("front_frame_id", "mid360_front_frame");
+    declare_parameter("front_imu_input_topic", "/livox/imu_192_168_1_188");
+    declare_parameter("front_imu_output_topic", "/mid360_front/imu");
     declare_parameter("back_input_topic", "/livox/lidar_192_168_1_182");
     declare_parameter("back_output_topic", "/mid360_back/lidar");
     declare_parameter("back_frame_id", "mid360_back_frame");
+    declare_parameter("back_imu_input_topic", "/livox/imu_192_168_1_182");
+    declare_parameter("back_imu_output_topic", "/mid360_back/imu");
 
     const auto front_input_topic = get_parameter("front_input_topic").as_string();
     const auto front_output_topic = get_parameter("front_output_topic").as_string();
     const auto front_frame_id = get_parameter("front_frame_id").as_string();
+    const auto front_imu_input_topic = get_parameter("front_imu_input_topic").as_string();
+    const auto front_imu_output_topic = get_parameter("front_imu_output_topic").as_string();
     const auto back_input_topic = get_parameter("back_input_topic").as_string();
     const auto back_output_topic = get_parameter("back_output_topic").as_string();
     const auto back_frame_id = get_parameter("back_frame_id").as_string();
+    const auto back_imu_input_topic = get_parameter("back_imu_input_topic").as_string();
+    const auto back_imu_output_topic = get_parameter("back_imu_output_topic").as_string();
 
-    const auto qos = rclcpp::SensorDataQoS();
+    const auto qos = 10;
     front_publisher_ = create_publisher<sensor_msgs::msg::PointCloud2>(front_output_topic, qos);
     back_publisher_ = create_publisher<sensor_msgs::msg::PointCloud2>(back_output_topic, qos);
+    front_imu_publisher_ = create_publisher<sensor_msgs::msg::Imu>(front_imu_output_topic, qos);
+    back_imu_publisher_ = create_publisher<sensor_msgs::msg::Imu>(back_imu_output_topic, qos);
 
     front_subscription_ = create_subscription<sensor_msgs::msg::PointCloud2>(
       front_input_topic, qos,
@@ -44,19 +55,45 @@ public:
         back_publisher_->publish(output);
       });
 
+    front_imu_subscription_ = create_subscription<sensor_msgs::msg::Imu>(
+      front_imu_input_topic, qos,
+      [this, front_frame_id](sensor_msgs::msg::Imu::ConstSharedPtr message) {
+        auto output = *message;
+        output.header.frame_id = front_frame_id;
+        front_imu_publisher_->publish(output);
+      });
+
+    back_imu_subscription_ = create_subscription<sensor_msgs::msg::Imu>(
+      back_imu_input_topic, qos,
+      [this, back_frame_id](sensor_msgs::msg::Imu::ConstSharedPtr message) {
+        auto output = *message;
+        output.header.frame_id = back_frame_id;
+        back_imu_publisher_->publish(output);
+      });
+
     RCLCPP_INFO(
       get_logger(), "Front: %s -> %s (frame_id=%s)",
       front_input_topic.c_str(), front_output_topic.c_str(), front_frame_id.c_str());
     RCLCPP_INFO(
       get_logger(), "Back: %s -> %s (frame_id=%s)",
       back_input_topic.c_str(), back_output_topic.c_str(), back_frame_id.c_str());
+    RCLCPP_INFO(
+      get_logger(), "Front IMU: %s -> %s (frame_id=%s)",
+      front_imu_input_topic.c_str(), front_imu_output_topic.c_str(), front_frame_id.c_str());
+    RCLCPP_INFO(
+      get_logger(), "Back IMU: %s -> %s (frame_id=%s)",
+      back_imu_input_topic.c_str(), back_imu_output_topic.c_str(), back_frame_id.c_str());
   }
 
 private:
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr front_publisher_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr back_publisher_;
+  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr front_imu_publisher_;
+  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr back_imu_publisher_;
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr front_subscription_;
   rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr back_subscription_;
+  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr front_imu_subscription_;
+  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr back_imu_subscription_;
 };
 
 int main(int argc, char * argv[])
